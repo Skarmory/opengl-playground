b7df2776cf4ba9bc3676b7c703a18486cf044616
Alastair Paterson <alas.paterson@gmail.com>
2017-03-01 01:19:01 +0000

    Rendering a rectangle
    - Created vertex array object, vertex buffer object, and element buffer object
    - Set the vertex attributes
    - Created simple vertex and fragment shader
    - Added call to draw the rectangle

--- OpenGL playground/src/main.cpp
+++ OpenGL playground/src/main.cpp
@@ -4,6 +4,41 @@
 
 #include <iostream>
 
+
+GLfloat vertices[] = {
+	0.5f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	-0.5f, 0.5f, 0.0f
+};
+
+GLuint indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+const GLchar* vertex_shader_source =
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}";
+
+const GLchar* fragment_shader_source =
+"#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}";
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main(void)
 {
 	glfwInit();
@@ -34,11 +69,70 @@ int main(void)
 
 	glViewport(0, 0, width, height);
 
+	glfwSetKeyCallback(window, key_callback);
+
+	// Create vertex shader
+	GLuint vertex_shader;
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	// Create fragment shader
+	GLuint fragment_shader;
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+
+	// Create shader program
+	GLuint shader_program;
+	shader_program = glCreateProgram();
+
+	// Attach and link shaders to shader program
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+
+	// Create a vertex array object and vertex buffer object
+	GLuint vao, vbo, ebo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+
+	// Set the element array buffer data
+	glBindVertexArray(vao);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Interpret vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw
+		glUseProgram(shader_program);
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		glfwSwapBuffers(window);
 	}
+
+	// Clean up
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
 
 	glfwTerminate();
 	return 0;
