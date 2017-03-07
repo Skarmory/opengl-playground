0e66ffd84666633670e528bbc95570a15bdb77cc
Alastair Paterson <alas.paterson@gmail.com>
2017-03-07 03:59:48 +0000

    Added a basic first person camera that can move about the scene

--- OpenGL playground/src/main.cpp
+++ OpenGL playground/src/main.cpp
@@ -78,10 +78,94 @@ GLuint indices[] = {
 	1, 2, 3
 };
 
+glm::vec3 camera_position = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 camera_front = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 camera_up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool keys[1024];
+bool first_mouse_input = true;
+
+GLfloat delta_time = 0.0f;
+GLfloat last_frame = 0.0f;
+
+GLfloat last_x = WIDTH / 2.0f;
+GLfloat last_y = HEIGHT / 2.0f;
+
+GLfloat pitch = 0.0f;
+GLfloat yaw = -90;
+
+GLfloat fov = 45.0f;
+
+void move()
+{
+	GLfloat camera_speed = 5.0f * delta_time;
+
+	if (keys[GLFW_KEY_W])
+		camera_position += camera_speed * camera_front;
+	if (keys[GLFW_KEY_S])
+		camera_position -= camera_speed * camera_front;
+	if (keys[GLFW_KEY_A])
+		camera_position -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+	if (keys[GLFW_KEY_D])
+		camera_position += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+		glfwSetWindowShouldClose(window, GL_TRUE);	
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (first_mouse_input)
+	{
+		last_x = xpos;
+		last_y = ypos;
+		first_mouse_input = false;
+	}
+
+	GLfloat xoffset = xpos - last_x;
+	GLfloat yoffset = last_y - ypos;
+	last_x = xpos;
+	last_y = ypos;
+
+	GLfloat sensetivity = 0.05f;
+	xoffset *= sensetivity;
+	yoffset *= sensetivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	front.y = sin(glm::radians(pitch));
+	front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+
+	camera_front = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
 }
 
 int main(void)
@@ -112,9 +196,12 @@ int main(void)
 
 	glViewport(0, 0, WIDTH, HEIGHT);
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glEnable(GL_DEPTH_TEST);
 
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	const GLchar* vpath = "../shaders/vshader.vs";
 	const GLchar* fpath = "../shaders/fshader.frag";
@@ -178,15 +265,14 @@ int main(void)
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glm::mat4 view;
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-	glm::mat4 projection;
-	projection = glm::perspective(glm::radians(45.0f), (GLfloat)WIDTH/(GLfloat)HEIGHT, 0.1f, 100.0f);
-
 	while (!glfwWindowShouldClose(window))
 	{
+		GLfloat current_frame = glfwGetTime();
+		delta_time = current_frame - last_frame;
+		last_frame = current_frame;
+
 		glfwPollEvents();
+		move();
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -204,6 +290,19 @@ int main(void)
 
 		glBindVertexArray(vao);
 
+		GLfloat radius = 10.f;
+		GLfloat cam_x = sin(glfwGetTime()) * radius;
+		GLfloat cam_z = cos(glfwGetTime()) * radius;
+
+		glm::mat4 view;
+		view = glm::lookAt(camera_position, camera_position + camera_front, camera_up);
+
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(fov), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+		glUniformMatrix4fv(glGetUniformLocation(shader_program.program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shader_program.program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
 		for (GLuint i = 0; i < sizeof(cube_positions)/sizeof(glm::vec3); i++)
 		{
 			glm::mat4 model;
@@ -211,9 +310,7 @@ int main(void)
 			model = glm::rotate(model, glm::radians((GLfloat)i * 20.f), glm::vec3(1.0f, 0.3f, 0.5f));
 
 			glUniformMatrix4fv(glGetUniformLocation(shader_program.program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-			glUniformMatrix4fv(glGetUniformLocation(shader_program.program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-			glUniformMatrix4fv(glGetUniformLocation(shader_program.program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
+			
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 		
